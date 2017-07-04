5df7a345ae48d435d2b78ff94c8b90ab416661e7
amadeus <dmit.karpovich@gmail.com>
2017-07-04 18:23:41 +0300

    color and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,70 @@
 $(function() {
-  $( ".currency-header" ).scrollLeft( scr );
-
-  var scr = 120;
-
-  $(".currency-header__item").on("swipeleft",function(){
-    $( ".currency-header" ).scrollLeft( scr );
-    scr += 120;
-    console.log(scr);
-  });
-
-
-  $(".currency-header__item").on("swiperight",function(){
-    // $(".currency-header").animate({ scrollLeft: $(this).scrollLeft(120) }, duration);
-    $( ".currency-header" ).scrollLeft( scr );
-    scr -= 120;
-    console.log(scr);
-  });
+  // $( ".currency-header" ).scrollLeft( scr );
+  //
+  // var scr = 120;
+  //
+  // $(".currency-header__item").on("swipeleft",function(){
+  //   $( ".currency-header" ).scrollLeft( scr );
+  //   scr += 120;
+  //   console.log(scr);
+  // });
+  //
+  //
+  // $(".currency-header__item").on("swiperight",function(){
+  //   // $(".currency-header").animate({ scrollLeft: $(this).scrollLeft(120) }, duration);
+  //   $( ".currency-header" ).scrollLeft( scr );
+  //   scr -= 120;
+  //   console.log(scr);
+  // });
   // $("p").on("swiperight",function(){
   //   alert("You swiped right!");
   // });
+
+  $("#demo").dragend({
+    afterInitialize: function() {
+      this.container.style.visibility = "visible";
+    },
+    onSwipeEnd: function() {
+      var first = this.pages[0],
+          last = this.pages[this.pages.length - 1];
+
+      $(".prev, .next").removeClass("deactivated");
+      $(".nav li").removeClass("active");
+
+      if (first === this.activeElement) {
+        $(".prev").addClass("deactivated")
+      };
+
+      if (last === this.activeElement) {
+        $(".next").addClass("deactivated")
+      }
+
+      $(".nav li").eq(this.page).addClass("active");
+
+    }
+  });
+
+  $(".prev").click(function() {
+    $("#demo").dragend("right");
+  });
+
+  $(".next").click(function() {
+    $("#demo").dragend("left");
+  });
+
+  $(".nav").click(function() {
+    var page = $(event.target).data("page");
+
+    $("#demo").dragend({
+      scrollToPage: page
+    });
+
+    $(event.target).addClass("active");
+
+  })
+
+
+
+
 
 });

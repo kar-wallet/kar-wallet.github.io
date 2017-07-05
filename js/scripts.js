f3fe1f32938b3d8c3c5253abcc143c4e4d12ba26
amadeus <dmit.karpovich@gmail.com>
2017-07-05 11:43:50 +0300

    fixed js

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ $(function() {
   //   alert("You swiped right!");
   // });
 
-  $("#demo").dragend({
+  $(".demoId").dragend({
     afterInitialize: function() {
       this.container.style.visibility = "visible";
     },
@@ -45,22 +45,65 @@ $(function() {
   });
 
   $(".prev").click(function() {
-    $("#demo").dragend("right");
+    $(".demoId").dragend("right");
   });
 
   $(".next").click(function() {
-    $("#demo").dragend("left");
+    $(".demoId").dragend("left");
   });
 
   $(".nav").click(function() {
     var page = $(event.target).data("page");
 
-    $("#demo").dragend({
+    $(".demoId").dragend({
       scrollToPage: page
     });
 
     $(event.target).addClass("active");
 
   });
+
+  // $("#demo").dragend({
+  //   afterInitialize: function() {
+  //     this.container.style.visibility = "visible";
+  //   },
+  //   onSwipeEnd: function() {
+  //     var first = this.pages[0],
+  //         last = this.pages[this.pages.length - 1];
+  //
+  //     $(".prev, .next").removeClass("deactivated");
+  //     $(".nav li").removeClass("active");
+  //
+  //     if (first === this.activeElement) {
+  //       $(".prev").addClass("deactivated")
+  //     };
+  //
+  //     if (last === this.activeElement) {
+  //       $(".next").addClass("deactivated")
+  //     }
+  //
+  //     $(".nav li").eq(this.page).addClass("active");
+  //
+  //   }
+  // });
+  //
+  // $(".prev").click(function() {
+  //   $("#demo").dragend("right");
+  // });
+  //
+  // $(".next").click(function() {
+  //   $("#demo").dragend("left");
+  // });
+  //
+  // $(".nav").click(function() {
+  //   var page = $(event.target).data("page");
+  //
+  //   $("#demo").dragend({
+  //     scrollToPage: page
+  //   });
+  //
+  //   $(event.target).addClass("active");
+  //
+  // });
 
 });

dfa3ff4bf6290a6d853e1727709829d38d63eb26
Guest <dima>
2017-07-06 14:36:23 +0300

    owl fixed

--- js/scripts.js
+++ js/scripts.js
@@ -55,7 +55,8 @@ $(function() {
       };
 
       if (last === this.activeElement) {
-        $(".next").addClass("deactivated")
+        // $(".next").addClass("deactivated")
+        this.jumpToPage(1);
       }
 
       $(".nav li").eq(this.page).addClass("active");
@@ -82,47 +83,56 @@ $(function() {
 
   });
 
-  // $("#demo").dragend({
-  //   afterInitialize: function() {
-  //     this.container.style.visibility = "visible";
-  //   },
-  //   onSwipeEnd: function() {
-  //     var first = this.pages[0],
-  //         last = this.pages[this.pages.length - 1];
-  //
-  //     $(".prev, .next").removeClass("deactivated");
-  //     $(".nav li").removeClass("active");
-  //
-  //     if (first === this.activeElement) {
-  //       $(".prev").addClass("deactivated")
-  //     };
-  //
-  //     if (last === this.activeElement) {
-  //       $(".next").addClass("deactivated")
-  //     }
-  //
-  //     $(".nav li").eq(this.page).addClass("active");
-  //
-  //   }
-  // });
-  //
-  // $(".prev").click(function() {
-  //   $("#demo").dragend("right");
-  // });
-  //
-  // $(".next").click(function() {
-  //   $("#demo").dragend("left");
-  // });
-  //
-  // $(".nav").click(function() {
-  //   var page = $(event.target).data("page");
-  //
-  //   $("#demo").dragend({
-  //     scrollToPage: page
-  //   });
-  //
-  //   $(event.target).addClass("active");
-  //
-  // });
 
+
+var resim = document.getElementById('ok');
+Hammer(resim).on('swipeleft', function(ev){
+    console.log('left: ', ev);
+    $(".prev").click();
 });
+Hammer(resim).on('swiperight', function(ev){
+    console.log('left: ', ev);
+    $(".next").click();
+});
+    
+
+    
+//var indexBeforeChange = -1;
+//$(function () {
+//    if ($('#slider .item').length > 1) {
+//        $('#slider').owlCarousel({
+//            onDrag: slideBeforeChange,
+//            onTranslated: slideChanged
+//        });
+//    }
+//});
+//function slideBeforeChange(event) {
+//    indexBeforeChange = event.page.index;
+//    console.log('indexBeforeChange: ' + indexBeforeChange);
+//}
+//function slideChanged(event) {
+//    var indexAfterChange = event.page.index;
+//    if (indexAfterChange != indexBeforeChange) {
+//        console.log('Slide changed, indexAfterChange: ' + indexAfterChange);
+//    } else {
+//        console.log('Slide did not change');
+//    }
+//}
+//    
+    
+//var owl = $('.nonloop').data('owlCarousel');
+//owl.on("drag.owl.carousel", function (event) {
+//    console.log(event.relatedTarget.state.direction);
+//});
+    
+});
+
+// var myElement = document.getElementsByClassName("dragend-page");
+// create a simple instance
+// by default, it only adds horizontal recognizers
+// var mc = new Hammer(myElement);
+
+// listen to events...
+// mc.on("panleft panright tap press", function(ev) {
+//     myElement.textContent = ev.type +" gesture detected.";
+// });
